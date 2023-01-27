deffa057431f2f763aa6ce1f5eaf62cc295334f4
KaisuHa <kaisu_h@yahoo.com>
2023-01-27 14:29:20 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+
+
+
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const addProduct = () => {
+    setData([...data, {key: text}]);
+    setText('');
+  }
+
+  const clearList = (data) => {
+      setData('');
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={{width: 200, borderWidth: 1, marginTop: 250, marginBottom: 50}} 
+      value = {text}
+      onChangeText = {text => setText(text)}
+      />
       <StatusBar style="auto" />
+      <View style={{flexDirection:'row', marginBottom: 10}}>
+      <Button title="ADD" onPress={addProduct} /> 
+      <Button title="CLEAR" onPress={()=>clearList(data)}/> 
+      </View>
+      <Text>Shopping list</Text>
+      <FlatList
+      data={data}
+      renderItem = {({ item }) => <Text> { item.key }</Text>}
+      />
     </View>
   );
 }
